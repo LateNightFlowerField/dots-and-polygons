9dc05f86c934eea7522b6108b954ef8ad8c9d2ce
LateNight FlowerField <eqemmett@gmail.com>
2023-06-18 12:33:52 -0400

    Start Oof V2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functools import cmp_to_key
-import pygame
+import pygame as pg
+from pygame import Vector2 as Vec2
 import numpy as np
 from dataclasses import dataclass
 from dataclasses import field
@@ -8,20 +9,20 @@ import random
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
-BACKGROUND_COLOR = pygame.Color(80,80,80)
+BACKGROUND_COLOR = pg.Color(80,80,80)
 FRAMERATE = 60
 GAME_WIDTH = 5
 GAME_HEIGHT = 5
 DOT_SPACING = 80
 DOT_RADIUS = 4
-DOT_COLOR = pygame.Color(90,20,60,a=255)
-SNAP_RANGE_COLOR = pygame.Color(70,70,70,a=80)
-DOT_NEAR_COLOR = pygame.Color(230,90,180)
-DOT_SLECTED_COLOR = pygame.Color(230,200,180)
-TEMP_LINE_COLOR = pygame.Color(60,10,30)
+DOT_COLOR = pg.Color(90,20,60,a=255)
+SNAP_RANGE_COLOR = pg.Color(70,70,70,a=80)
+DOT_NEAR_COLOR = pg.Color(230,90,180)
+DOT_SLECTED_COLOR = pg.Color(230,200,180)
+TEMP_LINE_COLOR = pg.Color(60,10,30)
 MOUSE_SNAP_DISTANCE = DOT_SPACING//2
 LINE_THICKNESS = 3
-LINE_COLOR = pygame.Color(160,100,130)
+LINE_COLOR = pg.Color(160,100,130)
 
 
 #https://stackoverflow.com/a/68924847 This works now after change < 0 to <= 0.
@@ -34,7 +35,7 @@ def line_intersection(x1, y1, x2, y2, x3, y3, x4, y4):
     py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom
     if (px - x1) * (px - x2) <= 0 and (py - y1) * (py - y2) <= 0 \
       and (px - x3) * (px - x4) <= 0 and (py - y3) * (py - y4) <= 0:
-        return pygame.Vector2(px,py)
+        return Vec2(px,py)
     else:
         return None
 
@@ -68,18 +69,19 @@ class Game():
     lines: list = field(default_factory=list)
     checking_lines: list = field(default_factory=list)
     last_line: list = field(default_factory=list)
+    flags: list = field(default_factory=list)
     add_point: bool = False
     add_line: bool = False
     draw_temp_line: bool = False
     selected_point: object = None
 
 def init():
-    pygame.init()
-    pygame.font.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    dots_layer = pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
-    line_layer = pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
+    pg.init()
+    pg.font.init()
+    screen = pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    dots_layer = pg.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
+    line_layer = pg.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pg.time.Clock()
     running = True
     check_polygon = False
     game_dots = list()
@@ -87,11 +89,106 @@ def init():
     y_padding = (SCREEN_HEIGHT-((GAME_HEIGHT-1)*DOT_SPACING))//2
     for x in range(GAME_WIDTH):
         for y in range(GAME_HEIGHT):
-            game_dots.append(pygame.Vector2(x*DOT_SPACING+x_padding,y*DOT_SPACING+y_padding))
+            game_dots.append(Vec2(x*DOT_SPACING+x_padding,y*DOT_SPACING+y_padding))
     return Game(screen, dots_layer, line_layer, clock, running, check_polygon, game_dots)
+
+def EventHandeler():
+    for event in pg.event.get(): #Handle Inputs
+        if event.type == pg.QUIT:
+            game.running = False
+        if event.type == pg.MOUSEBUTTONDOWN:
+            left, middle, right = pg.mouse.get_pressed()
+            if left and game.selected_point:
+                game.flags.append("line")
+            elif left:
+                game.flags.append("point")
+
+def RenderNearCircle():
+    for point in game.game_dots:
+        if point.distance_to(game.mouse_cords) < MOUSE_SNAP_DISTANCE:
+            pg.draw.circle(game.screen,SNAP_RANGE_COLOR,point,MOUSE_SNAP_DISTANCE)
+
+def RenderLines():
+    if game.selected_point: # Temporary Line
+        for point in game.game_dots:
+            if point.distance_to(game.mouse_cords) < MOUSE_SNAP_DISTANCE:
+                pg.draw.line(game.screen,TEMP_LINE_COLOR,game.selected_point,point,LINE_THICKNESS)
+                break
+        else:
+            pg.draw.line(game.screen,TEMP_LINE_COLOR,game.selected_point,game.mouse_cords,LINE_THICKNESS)
+
+    for line in game.lines:
+        pg.draw.line(game.screen,LINE_COLOR, *line, LINE_THICKNESS)
+    
+def RenderPoints():
+    for point in game.game_dots:
+        snap = point.distance_to(game.mouse_cords) < MOUSE_SNAP_DISTANCE
+        if point == game.selected_point:
+            pg.draw.circle(game.screen,DOT_SLECTED_COLOR,point,DOT_RADIUS)
+            continue
+        elif snap:
+            pg.draw.circle(game.screen,DOT_NEAR_COLOR,point,DOT_RADIUS)
+            continue
+        pg.draw.circle(game.screen,DOT_COLOR,point,DOT_RADIUS)
+
+def AddPoint():
+    if game.selected_point:
+        return False
+    for point in game.game_dots:
+        if point.distance_to(game.mouse_cords) < MOUSE_SNAP_DISTANCE:
+            game.selected_point = point
+            return True
+    return False
+
+def AddLine():
+    point1, point2 = game.selected_point, None
+    for point in game.game_dots:
+        if point.distance_to(game.mouse_cords) < MOUSE_SNAP_DISTANCE:
+            point2 = point
+            break
+    else:
+        return False
+    new_lines = [(point1,point2)]
+    new_lines = SubtractExistingSegments(new_lines)
+    # new_lines, updated_game_lines = DivideLines(new_lines)
+    game.lines.extend(new_lines)
+
+def SubtractExistingSegments(lines):
+    addedsegment = lines[0]
+    adjustedline = (Vec2(0.0,0.0), addedsegment[1] - addedsegment[0])
+    linesinline = []
+    for line in game.lines:
+        checkline = (Vec2(0.0,0.0),line[1] - line[0])
+        if adjustedline[1].normalize() in [checkline[1].normalize(),(checkline[1]*-1).normalize()]:
+            print(line)
+        
+    return lines
+
+def OnScreenDebugger():
+    font = pg.font.Font(None, 64)
+    text = font.render(str(game.mouse_cords), True, (10, 10, 10))
+    game.screen.blit(text, (0,0))
+    text = font.render(str(round(game.clock.get_fps())), True, (5, 5, 5))
+    game.screen.blit(text, (0,40))
+
 
 def sort_points_by_distance(point_1):
     return lambda point_2: np.sqrt((point_1.x-point_2.x)**2+(point_1.y-point_2.y)**2)
+
+def closet_point_to_corner(points):
+    print(points)
+    gamecorners = [Vec2(0,0),Vec2(0,SCREEN_HEIGHT),Vec2(SCREEN_WIDTH,0),Vec2(SCREEN_WIDTH,SCREEN_HEIGHT)]
+    smallest_point = None
+    smallest_point_distance = -1
+    for point in points:
+        distance_to_closet_corner = min([p.distance_to(point) for p in gamecorners])
+        print("ffd")
+        if distance_to_closet_corner < smallest_point_distance or smallest_point_distance == -1:
+            smallest_point = point
+            smallest_point_distance = distance_to_closet_corner
+    return smallest_point
+    # return smallest_point
+    
 
 def unique(input_list):
     """Returns the unique elements of a list"""
@@ -118,153 +215,49 @@ def standerdize_line(line):
     else:
         return (point_2,point_1)
 
+def sortlines(point):
+    return lambda line: max([(point.distance_to(line[0]),point.distance_to(line[0]))])*(line[0]-point+line[0]-point).normalize()
+
+def float_between(number, bound1, bound2):
+    if bound2 < bound1:
+        bound1, bound2 = bound2, bound1
+    return number == np.clip(number,bound1,bound2) 
+
+
+def point_in_bbox(point, bbox):
+    if not float_between(point.x,bbox[0].x,bbox[1].x):
+        return False
+    if not float_between(point.y,bbox[0].y,bbox[1].y):
+        return False
+    else:
+        return True
+
 if __name__ == "__main__":
     game = init()
     while game.running:
-
-        game.mouse_cords = pygame.mouse.get_pos()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game.running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if game.selected_point:
-                    game.add_line = True
-                    print("Set Add Line Flag")
-                else:
-                    game.add_point = True
-                    print("Set Add Point Flag")
-        
         game.screen.fill(BACKGROUND_COLOR)
-        game.draw_temp_line = True
-
-        for point in game.game_dots: #TODO seperate into functions
-            if point.distance_to(game.mouse_cords) < MOUSE_SNAP_DISTANCE:
-                pygame.draw.circle(game.screen,SNAP_RANGE_COLOR,point,MOUSE_SNAP_DISTANCE)
-            if game.add_point:
-                if point.distance_to(game.mouse_cords) < MOUSE_SNAP_DISTANCE:
-                    game.selected_point = point
-                    print("Selected Point")
-            if game.add_line:
-                if point.distance_to(game.mouse_cords) < MOUSE_SNAP_DISTANCE and point != game.selected_point:
-                    if check_valid_line(game, point):
-                        subtract_lines = [] #Remove part from segement which already exists
-                        intial_line = [(point-game.selected_point).normalize()*(-1),(point-game.selected_point).normalize()]
-                        print(intial_line)
-                        for line in game.lines:
-                            p1, p2 = line
-                            p1 = p1 - game.selected_point
-                            p2 = p2 - game.selected_point
-                            if p1.length() == 0:
-                                p1 = point-game.selected_point
-                            if p2.length() == 0:
-                                p2 = point-game.selected_point
-                            if all([p.normalize() in intial_line for p in [p1,p2]]):
-                                subtract_lines.append(line)
-                        print(subtract_lines, " SUB")
-                        for line in subtract_lines:
-                            pass
-                        intersections = [point,game.selected_point]
-                        temp_lines = game.lines.copy()
-                        for line in game.lines:
-                            if res := line_intersection(*point,*game.selected_point,*line[0],*line[1]):
-                                intersections.append(res)
-                                temp_lines.extend(zip([line[0],res],[res,line[1]]))
-                                temp_lines.remove(line)
-                        temp_lines = unique(temp_lines)
-                        zt = []
-                        for u in temp_lines:
-                            if (u not in zt) and ((u[1],u[0]) not in zt) and (u[0] != u[1]):
-                                zt.append(u)
-                        temp_lines = zt
-                        # print(temp_lines)
-                        # input()
-                        intersections = unique(intersections)
-                        intersections.sort(key=sort_points_by_distance(game.selected_point))
-                        game.last_line = tuple(intersections[0:2])
-                        game.new_lines = list(zip(intersections,intersections[1:]))
-                        if not all(map(lambda nl: nl in temp_lines,game.new_lines)):
-                            game.lines = unique(temp_lines)
-                            game.lines.extend(game.new_lines)
-                            print("Added line")
-                            game.check_polygon = True
-                            game.new_intersections = intersections
-                        
+        game.mouse_cords = pg.mouse.get_pos()
+        
+        EventHandeler()
+        RenderNearCircle()
+        RenderLines()
+        RenderPoints()
+        for flag in game.flags:
+            match flag:
+                case "point":
+                    AddPoint()
+                case "line":
+                    AddLine()
                     game.selected_point = None
-            if game.selected_point:
-                if point.distance_to(game.mouse_cords) < MOUSE_SNAP_DISTANCE:
-                    pygame.draw.line(game.screen,TEMP_LINE_COLOR,game.selected_point,point,LINE_THICKNESS)
-                    game.draw_temp_line = False
+                case "polygon":
+                    pass
+                case _:
+                    pass
+            
+        OnScreenDebugger()
 
-        if game.selected_point and game.draw_temp_line:
-            pygame.draw.line(game.screen,TEMP_LINE_COLOR,game.selected_point,game.mouse_cords,LINE_THICKNESS)
+        game.flags = []
 
-        for start_line, end_line in game.lines:
-            TEMP_LINE_COLOR = LINE_COLOR
-            # print(standerdize_line((start_line,end_line)))
-            random.seed(start_line.x*start_line.y*end_line.x*end_line.y)
-            TEMP_LINE_COLOR = pygame.Color(random.randint(1,255),random.randint(1,255),random.randint(1,255))
-            pygame.draw.line(game.screen, TEMP_LINE_COLOR, start_line, end_line,LINE_THICKNESS)
-        
-        if game.check_polygon:
-            intersections_to_check_from = []
-            for intersection in game.new_intersections:
-                if len(list(filter(has_point(intersection),game.lines))) > 1:
-                    intersections_to_check_from.append(intersection)
-            for intersection in intersections_to_check_from:
-                connected_lines = list(filter(has_point(intersection),game.lines))
-                centered_vectors = subtract_center(intersection,connected_lines)
-                centered_vectors = list(map(list, centered_vectors))
-                temp_vectors = []
-                for elm in centered_vectors:
-                    for belm in elm:
-                        if belm != pygame.Vector2(0,0):
-                            temp_vectors.append(belm)
-                centered_vectors = temp_vectors
-                for line in list(filter(has_point(intersection),game.new_lines)):
-                    line = list(line)
-                    line.remove(intersection)
-                    # print(line[0]-intersection)
-                    # print(find_closest_vector(line[0],centered_vectors, False))
-            # print(intersections_to_check_from)
-            # for cross in game.new_intersections[1:-1]:
-            #     for line in filter(has_point(cross),game.new_lines):
-            #         ways_to_check.append(line[::-1] if line in ways_to_check else line)
-            # for way in ways_to_check:
-            #     connected_lines = list(filter(has_point(way[0]),game.lines))
-            #     connected_lines = [line for line in connected_lines if line != way and line != (way[1],way[0])]
-
-            #     game.checking_lines = connected_lines
-            #     print(way,list(map(lambda h: [u for u in h if u != way[0]],connected_lines)))
-
-            game.check_polygon = False
-        for point in game.game_dots:
-            CURRENT_DOT_COLOR = DOT_COLOR
-            if point.distance_to(game.mouse_cords) < MOUSE_SNAP_DISTANCE:
-                CURRENT_DOT_COLOR = DOT_NEAR_COLOR
-            if game.selected_point:
-                if game.selected_point == point:
-                    CURRENT_DOT_COLOR = DOT_SLECTED_COLOR
-            pygame.draw.circle(game.screen,CURRENT_DOT_COLOR,point,DOT_RADIUS)
-        
-        for intersection_point in game.new_intersections:
-            pygame.draw.circle(game.screen,'yellow',intersection_point,DOT_RADIUS)
-
-        for start_line, end_line in game.checking_lines:
-            pygame.draw.line(game.screen, 'white', start_line, end_line,LINE_THICKNESS)
-
-        if game.add_line:
-            game.selected_point = False
-        game.add_line = False
-        game.add_point = False
-
-        font = pygame.font.Font(None, 64)
-        text = font.render(str(game.mouse_cords), True, (10, 10, 10))
-        game.screen.blit(text, (0,0))
-        text = font.render(str(round(game.clock.get_fps())), True, (5, 5, 5))
-        game.screen.blit(text, (0,40))
-        # print(len(game.lines))
-        pygame.display.update()
+        pg.display.update()
         game.clock.tick(FRAMERATE)
-        # print(game.clock.get_fps())
-    pygame.quit()
+    pg.quit()
