dde0c18ceca363864124d82f58bce78371186f76
LateNight FlowerField <eqemmett@gmail.com>
2023-06-23 14:49:00 -0400

    Everything in the current form

--- messarnd.py
+++ messarnd.py
@@ -1,7 +1,11 @@
 import main
 from pygame import Vector2 as Vec2
+from main import Inbox
 
+# print(main.Line(Vec2(9,9),Vec2(9,9)) == (Vec2(9,9),Vec2(9,9)))
 
-assert main.XInbox(Vec2(10,0),(Vec2(0,0),Vec2(20,0)))
+# DEBUG: Inbox: [90, 170] | (<Vector2(90, 170)>, <Vector2(90, 90)>)
+# DEBUG: False
+print(Inbox(Vec2(90,170),(Vec2(90,170),Vec2(90,90))))
 
-print(main.Line(Vec2(9,9),Vec2(9,9)) == (Vec2(9,9),Vec2(9,9)))
+print(Inbox(Vec2(250,170),(Vec2(250,170),Vec2(250,90))))
